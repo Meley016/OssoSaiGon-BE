dfc21b374a3fe256fe043f703ded28217f191b8d
jeyley <abc160cba@gmail.com>
2025-10-27 02:39:06 +0700

    update/user, product

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const fs = require("fs");
 require("dotenv").config();
  
 const connectDB = require("./config/database");
- 
+  
 // === IMPORT ROUTES ===
 const authRoutes = require("./routes/auth");
 const productRoutes = require("./routes/products");
