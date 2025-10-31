347f19aae0705877dba6f7470aafee53154d7171
jeyley <abc160cba@gmail.com>
2025-10-31 14:39:07 +0700

    fix cors

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ app.use(
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-
+ 
 // === SESSION: DÙNG CHO ADMIN PANEL ===
 app.use(
   session({
