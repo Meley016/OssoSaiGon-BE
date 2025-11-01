fc702a68dddde827e3c0e0dbd3856fd4c04990d4
jeyley <abc160cba@gmail.com>
2025-11-01 15:32:23 +0700

    update/CORS

--- src/index.js
+++ src/index.js
@@ -79,8 +79,9 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-      mongoUrl: process.env.MONGODB_URI,
+      mongoUrl: process.env.MONGO_URI,
       collectionName: "sessions",
+      ttl: 24 * 60 * 60,
     }),
     cookie: {
       httpOnly: true,
