7e0edd56656952c523c5cc08f33811b826cff531
jeyley <abc160cba@gmail.com>
2025-11-04 05:43:35 +0700

    fix/email.js

--- src/utils/email.js
+++ src/utils/email.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT) || 587,
   secure: process.env.EMAIL_SECURE === "true", // false cho 587
-  requireTLS: true,
+  ignoreTLS: true,
     auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
