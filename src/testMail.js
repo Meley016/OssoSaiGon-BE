65a2818606f6340c4f1e26547f07b444dbb36154
jeyley <abc160cba@gmail.com>
2025-11-04 07:24:04 +0700

    fix/port-send-mail

--- src/testMail.js
+++ src/testMail.js
@@ -1,13 +1,10 @@
-// test-email.js
 require("dotenv").config();
 const { sendEmail } = require("./utils/email");
 
-sendEmail({
-  to: "abc160cba@gmail.com", // ← email nhận thử
-  subject: "Test Email từ Osso Saigon",
-  html: `
-    <h1>Xin chào!</h1>
-    <p>Email hệ thống hoạt động hoàn hảo!</p>
-    <p>Thời gian: ${new Date().toLocaleString("vi-VN")}</p>
-  `,
-}).catch(console.error);
+(async () => {
+  await sendEmail({
+    to: "abc160cba@gmail.com",
+    subject: "Test gửi Gmail qua Nodemailer",
+    html: "<h3>Xin chào từ Osso Saigon 💌</h3><p>Mail này gửi thành công!</p>",
+  });
+})();
