d433cc51085d221e75359e49a152fed35958bf7b
jeyley <abc160cba@gmail.com>
2025-11-04 07:56:31 +0700

    fix/port-send-mail

--- src/testMail.js
+++ src/testMail.js
@@ -5,6 +5,7 @@ const { sendEmail } = require("./utils/email");
   await sendEmail({
     to: "abc160cba@gmail.com",
     subject: "Test gửi Gmail qua Nodemailer",
-    html: "<h3>Xin chào từ Osso Saigon 💌</h3><p>Mail này gửi thành công!</p>",
+    templateName: "resetPassword",
+    variables: { time: new Date().toLocaleString() },
   });
 })();
