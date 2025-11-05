61980d5b410d72731d2e8ce9d751dc4330ffe67d
jeyley <abc160cba@gmail.com>
2025-11-05 16:27:29 +0700

    fix/route/payment

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -4,7 +4,7 @@ const { apiProtect } = require("../middlewares/auth");
 
 const paymentController = require("../controllers/paymentController");
 const vnpayController = require("../controllers/vnpayController");
-const paypalController = require("../controllers/paypalController");
+// const paypalController = require("../controllers/paypalController");
 
 // 🟢 Thanh toán tổng hợp (tùy phương thức)
 router.post("/create", apiProtect, paymentController.createPayment);
@@ -15,8 +15,8 @@ router.get("/vnpay-return", vnpayController.vnpayReturn); // ✅ returnUrl (redi
 router.get("/vnpay-ipn", vnpayController.vnpayIPN); // ✅ ipnUrl (VNPay gọi server)
 
 // ================== PayPal ==================
-router.post("/paypal", apiProtect, paypalController.createPayment);
-router.get("/paypal/success", paypalController.success);
-router.get("/paypal/cancel", paypalController.cancel);
+// router.post("/paypal", apiProtect, paypalController.createPayment);
+// router.get("/paypal/success", paypalController.success);
+// router.get("/paypal/cancel", paypalController.cancel);
 
 module.exports = router;
