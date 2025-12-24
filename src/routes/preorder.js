c5d4d91e3635be37e6278d807c7185b1b052bb03
jeyley <abc160cba@gmail.com>
2025-12-24 14:14:07 +0700

    feat/user/pre-order/view

--- src/routes/preorder.js
+++ src/routes/preorder.js
@@ -6,6 +6,20 @@ const Preorder = require("../models/Preorder");
 
 router.post("/", apiProtect, createPreorder);
 
+router.get("/my", apiProtect, async (req, res) => {
+  try {
+    const preorders = await Preorder.find({
+      "user.email": req.user.email,
+    })
+      .populate("productId", "name images")
+      .sort({ createdAt: -1 });
+
+    res.json(preorders);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
 router.patch("/:id/contacted", apiProtect, async (req, res) => {
   try {
     const preorder = await Preorder.findById(req.params.id);
